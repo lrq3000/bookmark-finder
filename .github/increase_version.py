fffbbdceb7346e0f6fda4adb25551dd570194ff4
Xiaohan Tian <xiaohan.tian@gmail.com>
2024-02-09 16:32:33 -0800

    fixed minor issues in publish.yml

--- .github/increase_version.py
+++ .github/increase_version.py
@@ -7,11 +7,13 @@ with open(file_path, 'r') as file:
 
 for i, line in enumerate(lines):
     if line.strip().startswith('"version":'):
-        version_info = re.search(r'\"(\d+), (\d+), (\d+)\"', line)
+        print("i =", i)
+        version_info = re.search(r'"version":\s*"(\d+)\.(\d+)\.(\d+)"', line)
+        print("version_info =", version_info)
         if version_info:
             major, minor, subminor = map(int, version_info.groups())
             subminor += 1  # Increment the subminor version
-            new_version = f'    "version": ({major}, {minor}, {subminor}),\n'
+            new_version = f'  "version": "{major}.{minor}.{subminor}",\n'
             lines[i] = new_version
             break
 
