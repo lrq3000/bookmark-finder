cbc09befd20340588f5d9537013efe1556990f54
Stephen L. <LRQ3000@gmail.com>
2025-03-19 23:51:19 +0100

    fix: pruning implementation now works + fix regression in results window staying empty
    Signed-off-by: Stephen L. <LRQ3000@gmail.com>

--- src/bm_pruner.js
+++ src/bm_pruner.js
@@ -22,7 +22,6 @@ const pruneBookmarks = async () => {
     console.log(`starting prune`);
 
     document.querySelector("#btn_start_index").disabled = "true";
-    document.querySelector(".progress-container").style.visibility = "visible";
     document.querySelectorAll(`#hint_done`).forEach(e => e.style.display = `none`);
     document.querySelectorAll(`#hint_in_progress`).forEach(e => e.style.display = `block`);
     document.querySelectorAll(`#hint_prune_done`).forEach(e => e.style.display = `none`);
@@ -31,27 +30,31 @@ const pruneBookmarks = async () => {
     const response = await chrome.runtime.sendMessage({ req: "getIndexedBookmarks" });
     const indexedBookmarks = response.bms;
     console.log(`indexedBookmarks:`, indexedBookmarks);
+    document.querySelector(".progress-container").style.visibility = "visible";
+
 
     let prunedCount = 0;
 
-    for (let i = 0; i < indexedBookmarks.length; i++) {
-        const indexedBm = indexedBookmarks[i];
-        const indexedUrl = indexedBm.url;
+    if (indexedBookmarks && Array.isArray(indexedBookmarks)) {
+        for (let i = 0; i < indexedBookmarks.length; i++) {
+            const indexedBm = indexedBookmarks[i];
+            const indexedUrl = indexedBm.url;
 
-        // check if bookmark still exists
-        const bookmarks = await chrome.bookmarks.search({ url: indexedUrl });
-        if (bookmarks.length === 0) {
-            console.log(`Stale bookmark found: ${indexedUrl}`);
+            // check if bookmark still exists
+            const bookmarks = await chrome.bookmarks.search({ url: indexedUrl });
+            if (bookmarks.length === 0) {
+                console.log(`Stale bookmark found: ${indexedUrl}`);
 
-            // remove stale bookmark from index and bms array in background.js
-            const pruneResponse = await chrome.runtime.sendMessage({ req: "prune", url: indexedUrl });
-            if (pruneResponse.success) {
-                prunedCount += pruneResponse.count;
+                // remove stale bookmark from index and bms array in background.js
+                const pruneResponse = await chrome.runtime.sendMessage({ req: "prune", url: indexedUrl });
+                if (pruneResponse.success) {
+                    prunedCount += pruneResponse.count;
+                }
             }
-        }
 
-        // Update progress bar
-        updateProgress(Math.round((i + 1) * 100 / indexedBookmarks.length));
+            // Update progress bar
+            updateProgress(Math.round((i + 1) * 100 / indexedBookmarks.length));
+        }
     }
 
     document.querySelectorAll(`#hint_in_progress`).forEach(e => e.style.display = `none`);
